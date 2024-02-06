6162b79036e3d3fe3cab02fe7164fc4b2cd21748
Chad Dorsey <cdorsey@concord.org>
2024-02-06 14:33:38 -0500

    Added changes for environment variables

--- _incl/db-connect.php
+++ _incl/db-connect.php
@@ -1,13 +1,36 @@
 <?php
 
-$db_user = 'root'; // add user
-$db_pw = 'root'; // add password
-$db_name = 'dset-app-mysql2';
-$db_host = 'localhost'; // add db host
+$instance_host = getenv('INSTANCE_HOST');
+$db_port = getenv('DB_PORT');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_root_cert = getenv('DB_ROOT_CERT');
+$db_cert = getenv('DB_CERT');
+$db_key = getenv('DB_KEY');
+$private_ip = getenv('PRIVATE_IP');
 
-$dbh = new mysqli($db_host, $db_user, $db_pw, $db_name);
-if ($dbh->connect_error) {
-  die('Connection failed: ' . $dbh->connect_error);
+if ($private_ip === 'TRUE') {
+  $dbh = mysqli_init();
+  $dbh->ssl_set(
+      $db_key, // The path name to the key file
+      $db_cert, // The path name to the certificate file
+      $db_root_cert, // The path name to the certificate authority file
+      NULL, // The pathname to a directory that contains trusted SSL CA certificates in PEM format
+      NULL  // A list of allowable ciphers to use for SSL encryption
+  );
+  $success = $dbh->real_connect($instance_host, $db_user, $db_pass, $db_name, $db_port, NULL, MYSQLI_CLIENT_SSL);
+  
+  if (!$success) {
+      die('Connection failed: ' . $dbh->connect_error);
+  }
+} else {
+  // Fallback to non-SSL connection if PRIVATE_IP is not 'TRUE'
+  $dbh = new mysqli($instance_host, $db_user, $db_pass, $db_name, $db_port);
+  
+  if ($dbh->connect_error) {
+      die('Connection failed: ' . $dbh->connect_error);
+  }
 }
 
 ?>
